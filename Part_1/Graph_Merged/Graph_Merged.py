011a88ceec3a3229fc1506a58a4def080cafbb6f
Alessandro Zabeo <alessandro.zabeo6@gmail.com>
2024-02-03 19:24:51 +0100

    Fixed_path

--- Part_1/Graph_Merged/Graph_Merged.py
+++ Part_1/Graph_Merged/Graph_Merged.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
-df_15 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\Part_1\AM_1.5\AM 1.5.xlsx',
+df_15 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\34552-Photovoltaic-systems\Part_1\AM_1.5\AM 1.5.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col = 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
@@ -11,21 +11,21 @@ df_15 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignme
                   )
 
 
-df_3 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\Part_1\AM_3\AM 3.0.xlsx',
+df_3 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\34552-Photovoltaic-systems\Part_1\AM_3\AM 3.0.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col = 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
                     
                   )
 
-df_45 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\Part_1\AM_4.5\AM 4.5.xlsx',
+df_45 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\34552-Photovoltaic-systems\Part_1\AM_4.5\AM 4.5.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col = 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
                     
                   )
 
-df_6 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\Part_1\AM_6\AM 6.0.xlsx',
+df_6 = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\34552-Photovoltaic-systems\Part_1\AM_6\AM 6.0.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col = 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
