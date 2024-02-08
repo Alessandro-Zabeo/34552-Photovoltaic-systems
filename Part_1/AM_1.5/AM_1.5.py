8a1dfb550e18fee3b05c07b2cc1ec98769e6af5b
s230137 <s230137@student.dtu.dk>
2024-02-08 11:46:09 +0100

    Directories updated

--- Part_1/AM_1.5/AM_1.5.py
+++ Part_1/AM_1.5/AM_1.5.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as pyplot
 
-df = pd.read_excel (r'C:\Users\mscuc\Documents\GitHub\34552-Photovoltaic-systems\Part_1\AM_1.5\AM 1.5.xlsx',
+df = pd.read_excel ('AM 1.5.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col= 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
--- Part_1/AM_6/AM_6.py
+++ Part_1/AM_6/AM_6.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
-df = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\34552-Photovoltaic-systems\Part_1\AM_6\AM 6.0.xlsx',
+df = pd.read_excel ('AM 6.0.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col = 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
