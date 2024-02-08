660c3da5b07b510011428ffd0566b580204b8ef3
s230137 <s230137@student.dtu.dk>
2024-02-08 11:24:54 +0100

    Added puto

--- Part_1/AM_1.5/AM_1.5.py
+++ Part_1/AM_1.5/AM_1.5.py
@@ -2,17 +2,20 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as pyplot
 
-df = pd.read_excel (r'C:\Users\zabe1\Desktop\Photovoltaics Systems\3-Assignments\34552-Photovoltaic-systems\Part_1\AM_1.5\AM 1.5.xlsx',
+df = pd.read_excel (r'C:\Users\mscuc\Documents\GitHub\34552-Photovoltaic-systems\Part_1\AM_1.5\AM 1.5.xlsx',
                     sheet_name = 'Spectral irradiance',
                     index_col= 'Wavelength (nm)',
                     usecols=['Wavelength (nm)', 'Global to perpendicular plane  (W/m2/nm)','Global to horizontal plane  (W/m2/nm)','Direct to horizontal plane (W/m2/nm)','Diffuse to horizontal plane (W/m2/nm)'],
                     
                   )
 
-print(df)
-'''PART1'''
 
-# Porco putana
+#print(df)
+
+# puto el nombre de las columnas en minusculas
+
+
+'''PART1'''
 
 
 '''a) Global to perpendicular plane irradiance plot'''
