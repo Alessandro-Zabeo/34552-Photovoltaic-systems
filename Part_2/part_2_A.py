741ae4997b63dafe94b9a6cb492326b826d89e61
Alessandro Zabeo <s230142@dtu.dk>
2024-02-29 11:56:50 +0100

    Final_partA

--- Part_2/part_2_A.py
+++ Part_2/part_2_A.py
@@ -1,13 +1,10 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as pyplot
 import math
 
-beta = 54  # in degrees
-gamma = -91  # in degrees
-teta_z = 75  # in degrees
+beta = 60  # in degrees
+gamma = -180  # in degrees
+teta_z = 70  # in degrees
 alfa_s = 15  # in degrees
-gamma_s = 105  # in degrees
+gamma_s = 0  # in degrees
 
 # Convert angles to radians (necessary for math functions)
 beta_rad = math.radians(beta)
@@ -29,10 +26,14 @@ print(f"teta: {teta:.2f}")  # Print teta with 2 decimal places
 
 DNI = 600
 DHI = 120
-ro_g = 0,2
+ro_g = 0.2
 Beam_irradiance = DNI*math.cos(teta_rad)
 print(f"Beam irradiance: {Beam_irradiance:.2f}")  # Print teta with 2 decimal places
 
 Diffuse_irradiance = DHI * ((1+math.cos(beta_rad))/2)
 print(f"Diffuse irradiance: {Diffuse_irradiance:.2f}")  # Print teta with 2 decimal places
 
+GHI = DNI*math.cos(teta_z_rad) + DHI
+print(f"GHI: {GHI:.2f}")
+Ground_reflected = GHI*ro_g*(1-math.cos(beta_rad))/2
+print(f"Ground reflected: {Ground_reflected:.2f}")
